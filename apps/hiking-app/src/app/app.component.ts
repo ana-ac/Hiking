39655a4af3a48aaf3c46430e3265e00cd66e984b
Ana Arriaga <ana.arriaga@scalefast.com>
2021-12-11 22:08:34 +0100

    Creating nx workspace

---
+++ apps/hiking-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hiking-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hiking-app';
+}
